ab04821c9a775db0f8ecc176b97d048425d47f5a
George Dwyer <george.dwyer@ucl.ac.uk>
2018-01-15 11:22:46 +0000

    Moving the robot script working

--- comp313p_example/scripts/moving_the_robot.py
+++ comp313p_example/scripts/moving_the_robot.py
@@ -18,21 +18,21 @@ class stdr_controller():
         self.current_pose = data
 
     def run(self):
-        self.command_pose.x = self.current_pose.pose.pose.position.x
-        self.command_pose.y = self.current_pose.pose.pose.position.y
-
         while not rospy.is_shutdown():
             distance_tolerance = 0.01
             vel_msg = Twist()
 	    
-	    rospy.log_info('Current position, x: {}, y:{}'.format(self.current_pose.pose.pose.position.x, self.current_pose.pose.pose.position.y))
-	    lin_velocity = raw_input('Enter desired linear velocity: ')
-	    rot_velocity = raw_input('Enter desired rotational velocity: ')
-	    duration = raw_input('Enter desired rotation: ')
-	    vel_msg.linear.x = lin_velocity
+            rospy.loginfo('Current position, x: {}, y:{}'.format(self.current_pose.pose.pose.position.x, self.current_pose.pose.pose.position.y))
+            lin_velocity = float(raw_input('Enter desired linear velocity: '))
+            rot_velocity = float(raw_input('Enter desired rotational velocity: '))
+            duration = float(raw_input('Enter desired duration: '))
+            vel_msg.linear.x = lin_velocity
             vel_msg.angular.z = rot_velocity
             self.velocity_publisher.publish(vel_msg)
             rospy.sleep(duration)
+            vel_msg.linear.x = 0.0
+            vel_msg.angular.z = 0.0
+            self.velocity_publisher.publish(vel_msg)
 
    
 if __name__ == '__main__':
