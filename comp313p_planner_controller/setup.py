07e44ae78c9925060b265fcf7c41e398c3d28d14
Simon Julier <s.julier@ucl.ac.uk>
2018-01-17 18:58:01 +0000

    Added subdirectories as packages.

--- comp313p_planner_controller/setup.py
+++ comp313p_planner_controller/setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages = ['comp313p_planner_controller'],
+    packages = ['comp313p_planner_controller.controller',
+                    'comp313p_planner_controller.planner.algorithms',
+                    'comp313p_planner_controller.planner.base',
+                    'comp313p_planner_controller.planner.graphics'],
     packages_dir = {'': 'src'}
     )
 
