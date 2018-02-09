57387cf9c9f60d87963edcb9fb7b4a0fda04e0a5
Simon Julier <s.julier@ucl.ac.uk>
2018-02-09 19:34:01 +0000

    Added tool to monitor STDR output.

---
+++ comp313p_resources/scripts/robot_listener.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg  import Twist
+from nav_msgs.msg import Odometry
+from math import pow,atan2,sqrt,pi
+
+class RobotListener():
+
+    def __init__(self):
+        #Creating our node,publisher and subscriber
+        rospy.init_node('robot_listener', anonymous=True)
+        self.current_pose_subscriber = rospy.Subscriber('/robot0/odom', Odometry, self.current_callback)
+ 
+    def current_callback(self, odometry):
+
+        odometryPose = odometry.pose.pose
+
+        position = odometryPose.position
+        orientation = odometryPose.orientation
+        
+        theta = 2 * atan2(orientation.z, orientation.w)
+
+        rospy.loginfo("Current Pose: x: %f, y:%f, theta: %f", position.x, position.y, theta)
+
+    def run(self):
+
+        # Sleep for 1s before starting. This gives time for all the parts of stdr to start up
+        rospy.wait_for_service('/robot0/replace')
+        rospy.spin()
+   
+if __name__ == '__main__':
+    try:
+        #Testing our function
+        x = RobotListener()
+        x.run()
+
+    except rospy.ROSInterruptException:
+        pass
