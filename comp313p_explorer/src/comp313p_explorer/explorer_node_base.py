4612fdab7aa047511c467c1b80a94c2558d80242
Simon Julier <s.julier@ucl.ac.uk>
2018-03-05 18:01:02 +0000

    Tidied up creating the drawer. Added control via parameters.

--- comp313p_explorer/src/comp313p_explorer/explorer_node_base.py
+++ comp313p_explorer/src/comp313p_explorer/explorer_node_base.py
@@ -1,6 +1,8 @@
 import rospy
 from comp313p_mapper.msg import *
 from comp313p_reactive_planner_controller.srv import *
+from comp313p_reactive_planner_controller.occupancy_grid import OccupancyGrid
+from comp313p_reactive_planner_controller.grid_drawer import OccupancyGridDrawer
 
 class ExplorerNodeBase(object):
 
@@ -20,9 +22,16 @@ class ExplorerNodeBase(object):
         self.occupancyGrid = None
         self.deltaOccupancyGrid = None
 
+        self.showOccupancyGrid = rospy.get_param('show_explorer_occupancy_grid', True)
+        self.showDeltaOccupancyGrid = rospy.get_param('show_explorer_delta_occupancy_grid', True)
+
+        self.occupancyGridDrawer = None
+        self.deltaOccupancyGridDrawer = None
+        self.visualisationUpdateRequired = False
+       
     def mapUpdateCallback(self, msg):
         rospy.loginfo("map update received")
-
+        
         # If the occupancy grids do not exist, create them
         if self.occupancyGrid is None:
             self.occupancyGrid = OccupancyGrid.fromMapUpdateMessage(msg)
@@ -32,6 +41,9 @@ class ExplorerNodeBase(object):
         self.occupancyGrid.updateGridFromVector(msg.occupancyGrid)
         self.deltaOccupancyGrid.updateGridFromVector(msg.deltaOccupancyGrid)
 
+        # Flag there's something to show graphically
+        self.visualisationUpdateRequired = True
+
         # Update the frontiers
         #anyFrontiersRemaining = self.updateFrontiers()
 
@@ -39,7 +51,6 @@ class ExplorerNodeBase(object):
         #chooseNewDestination, newDestination = self.chooseNewDestination()
 
         # If a new destination is required, send it out
-
 
     # Simple helper to see if a cell is on a frontier or not
     def isFrontierCell(self, coords):
@@ -52,6 +63,34 @@ class ExplorerNodeBase(object):
     # explorer will exit.
     def updateFrontiers(self):
         pass
+
+    def updateVisualisation(self):
+
+        if self.visualisationUpdateRequired is False:
+            return
+
+        # Update the visualisation; note that we can only create the
+        # drawers here because we don't know the size of the map until
+        # we get the first update from the mapper.
+        if self.showOccupancyGrid is True:
+            if self.occupancyGridDrawer is None:
+                windowHeight = rospy.get_param('maximum_window_height_in_pixels', 600)
+                self.occupancyGridDrawer = OccupancyGridDrawer('Explorer Node Occupancy Grid',\
+                                                               self.occupancyGrid, windowHeight)
+	        self.occupancyGridDrawer.open()
+	    self.occupancyGridDrawer.update()
+
+        if self.showDeltaOccupancyGrid is True:
+            if self.deltaOccupancyGridDrawer is None:
+                windowHeight = rospy.get_param('maximum_window_height_in_pixels', 600)
+                self.deltaOccupancyGridDrawer = OccupancyGridDrawer('Explorer Node Delta Occupancy Grid',\
+                                                                    self.deltaOccupancyGrid, windowHeight)
+	        self.deltaOccupancyGridDrawer.open()
+	    self.deltaOccupancyGridDrawer.update()
+
+        self.visualisationUpdateRequired = False
     
     def run(self):
-        rospy.spin()
+        while not rospy.is_shutdown():
+            rospy.sleep(0.1)
+            self.updateVisualisation()
