9d88b7a9f1ab6f166f3786b30e5b71aaa9dd25ba
Simon Julier <s.julier@ucl.ac.uk>
2018-03-05 16:58:46 +0000

    Started implementing the explorer infrastructure. At the moment it just shows a new map has been received.

--- comp313p_explorer/src/comp313p_explorer/explorer_node.py
+++ comp313p_explorer/src/comp313p_explorer/explorer_node.py
@@ -1,9 +1,16 @@
-
+import rospy
+from comp313p_mapper.msg import *
 
 class ExplorerNode(object):
 
     def __init__(self):
-        pass
+        rospy.init_node('explorer')
 
+        # Subscribe to get the map update messages
+        self.mapUpdateSubscriber = rospy.Subscriber('updated_map', MapUpdate, self.mapUpdateCallback)
+
+    def mapUpdateCallback(self, msg):
+        rospy.loginfo("map update received")
+    
     def run(self):
-        pass
+        rospy.spin()
