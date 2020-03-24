ddc5f364b407a735eb38fdce2fdcb1b531128bfd
Simon Julier <s.julier@ucl.ac.uk>
2020-03-24 11:58:45 +0000

    Replaced my hacked 30s attempt with a proper solution.

---
+++ comp0037_reactive_planner_controller/src/comp0037_reactive_planner_controller/a_star_planner.py
@@ -0,0 +1,42 @@
+from dijkstra_planner import DijkstraPlanner
+import math
+
+# self class implements the A* search algorithm
+
+from enum import Enum
+
+class CostToComeHeuristic(Enum):
+    EUCLIDEAN = 1
+    MANHATTAN = 2
+    OCTILE = 3
+
+class AStarPlanner(DijkstraPlanner):
+    
+    def __init__(self, title, occupancyGrid):
+        DijkstraPlanner.__init__(self, title, occupancyGrid)
+        self.heuristic = CostToComeHeuristic.OCTILE
+        self.alpha = 1
+
+    def setHeuristc(self, heuristic):
+        self.heuristic = heuristic
+        
+    def setHeuristicWeight(self, alpha):
+        self.alpha = alpha
+
+    def computePriorityQueueKey(self, cell):
+        
+        # Compute the cost-to-go using a suitable heuristic
+        dXG = abs(cell.coords[0] - self.goal.coords[0])
+        dYG = abs(cell.coords[1] - self.goal.coords[1])
+ 
+        if self.heuristic is CostToComeHeuristic.EUCLIDEAN:
+            G = math.sqrt(dXG * dXG + dYG * dYG)
+        elif self.heuristic is CostToComeHeuristic.MANHATTAN:
+            G = dXG + dYG
+        elif self.heuristic is CostToComeHeuristic.OCTILE:
+            G = max(dXG, dYG) + (math.sqrt(2) -1) * min(dXG, dYG)
+ 
+        # Compute the cost using weighted A*
+        key = cell.pathCost + self.alpha * G
+
+        return key
